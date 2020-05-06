ea0e418e9ba28d297ebf4afcf23d70d938bf27a9
John Melody Me <johnmelodyme@yandex.com>
2020-05-07 06:43:37 +0800

    Updated Output { format}

--- src/__main.java
+++ src/__main.java
@@ -3,22 +3,22 @@ package src;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.Scanner;
+
 import model.__aes;
 
 public class __main {
-      private static String __secret_key, __user_input, __original_string, __encrypted_string, __decrypted_string;
+      private static String __secret_key, __user_input, __original_string;
+      private static String __encrypted_string, __decrypted_string;
       private static Scanner __get_user_input;
       private static File __file;
       private static FileWriter __fileWriter;
-      private static DateTimeFormatter __date_time_formatter;
-      private static LocalDateTime __now;
 
       public static void main(final String[] args) {
+            // :.. User input
             __welcome_method();
+
             // :... Configure
             __get_user_input = new Scanner(System.in);
             __secret_key = "this_is_a_secret_Key";
@@ -31,9 +31,6 @@ public class __main {
             // :...Save Output
             __save_output();
 
-            // :.. Get Current Date and Time
-            __date_time_formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-            __now = LocalDateTime.now();
       }
 
       private static void __welcome_method() {
@@ -41,7 +38,7 @@ public class __main {
       }
 
       private static void __print_out() {
-            System.out.println("Performed at " + java.time.LocalDate.now());
+            System.out.println("Performed at " + java.time.LocalDate.now() +" "+ java.time.LocalTime.now());
             System.out.println("The Secret Key: " + __secret_key);
             System.out.println("The Original Content: " + __original_string);
             System.out.println("The Encrypted Content: " + __encrypted_string);
@@ -51,14 +48,15 @@ public class __main {
 
       private static void __save_output() {
             try {
-                  __file = new File("output/" + new Random().nextInt(45) + "output.txt");
+                  __file = new File("output/"+ new Random().nextInt(10)+ "output.txt");
                   __fileWriter = new FileWriter(__file);
-                  __fileWriter.write("Performed at " + java.time.LocalDate.now() + "\n");
+                  __fileWriter.write("Performed at " + java.time.LocalDate.now() +" "+ java.time.LocalTime.now() + "\n\n");
                   __fileWriter.write("The Secret Key: " + __secret_key + "\n");
                   __fileWriter.write("The Original Content: " + __original_string + "\n");
                   __fileWriter.write("The Encrypted Content: " + __encrypted_string + "\n");
                   __fileWriter.write("The Decrypted Content: " + __decrypted_string + "\n");
                   __fileWriter.close();
+                  System.gc();
             } catch (final IOException ioException) {
                   System.out.println("Exception: " + ioException.toString());
             }
