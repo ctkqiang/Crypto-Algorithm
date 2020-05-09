dcdc3efd8ef3841ddd926d0fbd9391b6403a4844
John Melody Me <johnmelodyme@yandex.com>
2020-05-09 20:28:17 +0800

    updated

--- model/__sha_512_model.java
+++ model/__sha_512_model.java
@@ -1,5 +1,14 @@
 package model;
-
+/**
+ * Copyright 2020 © John Melody Me
+ * Licensed under the John Melody Me, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
