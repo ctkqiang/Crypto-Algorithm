6cdd6dd1369c61102553b55fdb141177944216c8
John Melody Me <johnmelodyme@yandex.com>
2020-05-13 01:52:36 +0800

    updates

--- src/encryption/__aes_encryption.dart
+++ src/encryption/__aes_encryption.dart
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2020 © John Melody Me
+ * Licensed under the John Melody Me, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import 'dart:io';
 import 'package:aes_crypt/aes_crypt.dart';
 
