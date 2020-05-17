5c75cefa134cb9dfafbdda055c7bbc70c6485a9a
John Melody Me <Johnmelodyme@yandex.com>
2020-05-18 05:20:19 +0800

    ...

---
+++ CryptographyProvider.java
@@ -0,0 +1,20 @@
+import java.security.Provider;
+import java.security.Security;
+
+public class CryptographyProvider {
+    public static Provider[] __provider;
+    private static String __name, __info;
+    private static double __version;
+
+    public static void main(final String[] args) {
+        __provider = Security.getProviders();
+
+        for (int i = 0; i < __provider.length; i++) {
+            __name = __provider[i].getName();
+            __info = __provider[i].getInfo();
+            __version = __provider[i].getVersion();
+            System.out.println("Providers [" + i + "] ==>" + __name + "[" 
+            + __version + "]" + " " + __info);
+        }
+    }
+}
